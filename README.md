be8fe91d79b95d484c982a72d214b495b64777db
George Gonzalez <george@mycopilot.com>
2021-09-30 19:15:53 -0700

    revamped

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# geckopedia
+# Appy Vue
+
+Vue.js landing page template designed by Cruip.
 
 ## Project setup
 ```
@@ -15,6 +17,11 @@ npm run serve
 npm run build
 ```
 
+### Run your tests
+```
+npm run test
+```
+
 ### Lints and fixes files
 ```
 npm run lint
@@ -22,3 +29,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Support notes
+We are shipping our templates with a very basic Vue.js configuration to let you quickly get into the development process, but we don't discourage you from using any other configuration or framework built on the top of Vue. So, please note that any request dealing with Vue (e.g. extra features, customisations, et cetera) is to be considered out of the support scope.
+
+For more information about what support covers, please see our (FAQs)[https://cruip.com/faq/].
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AOS from 'aos'
+import { focusHandling } from 'cruip-js-toolkit'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  mounted() {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 750,
+      easing: 'ease-out-quart',
+    })
+    focusHandling()
+    // Route change
+    if (this.$router) {
+      this.$watch('$route', () => {
+        focusHandling('outline')
+      });
+    }    
+  }  
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
